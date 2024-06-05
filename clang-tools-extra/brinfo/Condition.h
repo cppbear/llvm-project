71cf3d07a04505d0b0166cc0739a6cfcde4d242a
cppbear <chubei99@outlook.com>
2024-06-05 17:49:25 +0800

    implement for llvm::isa<>
    Signed-off-by: cppbear <chubei99@outlook.com>

--- clang-tools-extra/brinfo/Condition.h
+++ clang-tools-extra/brinfo/Condition.h
@@ -7,14 +7,19 @@ using namespace std;
 namespace BrInfo {
 
 inline void rtrim(string &S) {
-  S.erase(find_if(
-              S.rbegin(), S.rend(),
-              [](unsigned char Ch) { return !isspace(Ch) && Ch != ';'; })
+  S.erase(find_if(S.rbegin(), S.rend(),
+                  [](unsigned char Ch) { return !isspace(Ch) && Ch != ';'; })
               .base(),
           S.end());
 }
 
 class BaseCond {
+public:
+  enum CondKind { IF, CASE, DEFAULT, LOOP, TRY };
+
+private:
+  const CondKind Kind;
+
 protected:
   const Stmt *Cond = nullptr;
   string CondStr;
@@ -27,12 +32,13 @@ protected:
   void findCallExpr(const Stmt *S);
 
 public:
-  BaseCond(const Stmt *Cond) : Cond(Cond) {
+  BaseCond(CondKind K, const Stmt *Cond) : Kind(K), Cond(Cond) {
     findDeclRefExpr(Cond);
     findCallExpr(Cond);
     // setCondStr(Context);
   }
   virtual ~BaseCond() { Cond = nullptr; }
+  CondKind getKind() const { return Kind; }
   virtual void dump(const ASTContext *Context) = 0;
   virtual string toString(const ASTContext *Context) = 0;
   const Stmt *getCond() { return Cond; }
@@ -40,37 +46,35 @@ public:
   bool isNot() { return IsNot; }
   bool containDeclRefExpr() { return ContainDeclRefExpr; }
   bool containCallExpr() { return ContainCallExpr; }
-  vector<const DeclRefExpr *> &getDeclRefExprList() {
-    return DeclRefExprList;
-  }
+  vector<const DeclRefExpr *> &getDeclRefExprList() { return DeclRefExprList; }
   vector<const CallExpr *> &getCallExprList() { return CallExprList; }
   void setCondStr(const ASTContext *Context);
 };
 
 class IfCond : public BaseCond {
 public:
-  IfCond(const Stmt *Cond)
-      : BaseCond(Cond) {
+  IfCond(const Stmt *Cond) : BaseCond(IF, Cond) {
     // setCondStr(Context);
   }
   virtual ~IfCond() {}
   void dump(const ASTContext *Context) override;
-//   void setCondStr(const ASTContext &Context);
+  //   void setCondStr(const ASTContext &Context);
   string toString(const ASTContext *Context) override;
+  static bool classof(const BaseCond *Cond) { return Cond->getKind() == IF; }
 };
 
 class CaseCond : public BaseCond {
   Stmt *Case = nullptr;
 
 public:
-  CaseCond(Stmt *Cond, Stmt *Case)
-      : BaseCond(Cond), Case(Case) {
+  CaseCond(Stmt *Cond, Stmt *Case) : BaseCond(CASE, Cond), Case(Case) {
     // setCondStr(Context);
   }
   virtual ~CaseCond() { Case = nullptr; }
   void dump(const ASTContext *Context) override;
   void setCondStr(const ASTContext *Context){};
   string toString(const ASTContext *Context) override;
+  static bool classof(const BaseCond *Cond) { return Cond->getKind() == CASE; }
 };
 
 class DefaultCond : public BaseCond {
@@ -78,24 +82,28 @@ class DefaultCond : public BaseCond {
 
 public:
   DefaultCond(Stmt *Cond, vector<Stmt *> Cases)
-      : BaseCond(Cond), Cases(Cases) {
+      : BaseCond(DEFAULT, Cond), Cases(Cases) {
     // setCondStr(Context);
   }
   virtual ~DefaultCond() {}
   void dump(const ASTContext *Context) override;
   void setCondStr(const ASTContext *Context){};
   string toString(const ASTContext *Context) override;
+  static bool classof(const BaseCond *Cond) {
+    return Cond->getKind() == DEFAULT;
+  }
 };
 
 class LoopCond : public BaseCond {
 public:
-  LoopCond(Stmt *Cond) : BaseCond(Cond) {
+  LoopCond(Stmt *Cond) : BaseCond(LOOP, Cond) {
     // setCondStr(Context);
   }
   virtual ~LoopCond() {}
   void dump(const ASTContext *Context) override;
   void setCondStr(const ASTContext *Context){};
   string toString(const ASTContext *Context) override;
+  static bool classof(const BaseCond *Cond) { return Cond->getKind() == LOOP; }
 };
 
 class TryCond : public BaseCond {};
